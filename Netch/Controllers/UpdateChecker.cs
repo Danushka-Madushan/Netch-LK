40a0128de9dc5b16f2dfbde3fcfa52efd5006881
Danushka-Madushan <Danushkamadushan128@gmail.com>
2022-10-28 22:27:35 +0530

    Netch LK Official Custom Build

--- Netch/Controllers/UpdateChecker.cs
+++ Netch/Controllers/UpdateChecker.cs
@@ -15,7 +15,7 @@ public static class UpdateChecker
     public const string Name = @"Netch-LK";
     public const string Copyright = @"Copyright © 2019 - 2022";
 
-    public const string AssemblyVersion = @"1.9.7";
+    public const string AssemblyVersion = @"2.0.1";
     private const string Suffix = @"";
 
     public static readonly string Version = $"{AssemblyVersion}{(string.IsNullOrEmpty(Suffix) ? "" : $"-{Suffix}")}";
